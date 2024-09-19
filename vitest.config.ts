dfa104bb507e2d717886029f3e4613644c38b74a
Mahmudul Alam <mahmudul@ccprotocol.xyz>
2024-09-19 16:38:16 +0600

    test: fix path

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    include: ['**/*.{test,spec}.{js,ts,jsx,tsx}'],
+    include: ['src/**/*.{test,spec}.{js,ts,jsx,tsx}'],
   },
 });
